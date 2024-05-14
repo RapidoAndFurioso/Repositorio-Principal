34ed208668328d1088ab94c9ccfc1c27031b277c
am9DAM1 <am9DAM1@PC09DAM1.informatica.iesjc>
2024-05-14 09:13:59 +0200

    Solucion de TennisGame1.

---
+++ ExamenRefactorizar/src/Principal/TennisGame.java
@@ -0,0 +1,7 @@
+package Principal;
+
+
+public interface TennisGame {
+    void wonPoint(String playerName);
+    String getScore();
+}
--- ExamenRefactorizar/src/Principal/TennisGame1.java
+++ ExamenRefactorizar/src/Principal/TennisGame1.java
@@ -1,7 +1,7 @@
 package Principal;
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -13,65 +13,50 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             m_score1 += 1;
-        else
+        else if (playerName.equals("player2"))
             m_score2 += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (m_score1 == m_score2) {
+            return getEqualScore(m_score1);
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            return getAdvantageOrWinScore();
+        } else {
+            return getStandardScore();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
     }
-}//algo
+
+    private String getEqualScore(int score) {
+        switch (score) {
+            case 0: return "Love-All";
+            case 1: return "Fifteen-All";
+            case 2: return "Thirty-All";
+            default: return "Deuce";
+        }
+    }
+
+    private String getAdvantageOrWinScore() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) return "Advantage player1";
+        else if (minusResult == -1) return "Advantage player2";
+        else if (minusResult >= 2) return "Win for player1";
+        else return "Win for player2";
+    }
+
+    private String getStandardScore() {
+        return getScoreDescription(m_score1) + "-" + getScoreDescription(m_score2);
+    }
+
+    private String getScoreDescription(int score) {
+        switch (score) {
+            case 0: return "Love";
+            case 1: return "Fifteen";
+            case 2: return "Thirty";
+            case 3: return "Forty";
+            default: return "";
+        }
+    }
+}
